85a3307388efc6c32e26b97b239b3f53284b8724
Ricarda Erhart <ricarda.erhart@gmail.com>
2019-05-15 09:33:13 +0200

    improved print- and exon-function

--- assignment1.py
+++ assignment1.py
@@ -65,8 +65,8 @@ class Assignment1:
 
     def get_coordinates_of_gene(self):
         global UCSClist
-        print("Start: ", UCSClist[3])
-        print("Stop: ", UCSClist[4])
+        print("Startposition of Gene: ", UCSClist[3])
+        print("Stopposition of Gene: ", UCSClist[4])
 
     def get_gene_symbol(self):
         global UCSClist
@@ -74,7 +74,7 @@ class Assignment1:
 
     def get_sam_header(self):#
         samfile = pysam.AlignmentFile("chr21.bam", "rb")
-        print('Header: ', samfile.header)
+        print('Header of Samfile: ', samfile.header['HD'])#without ['HD'] for whole header
         samfile.close()
 
     def get_properly_paired_reads_of_gene(self):
@@ -86,7 +86,7 @@ class Assignment1:
         for read in samfile.fetch("chr21", Start, End):
             if read.is_proper_pair == True:
                 count=count+1
-        print('Properly Paired Reads: ', count)
+        print('Number of Properly Paired Reads: ', count)
         samfile.close()
 
     def get_gene_reads_with_indels(self):
@@ -94,7 +94,6 @@ class Assignment1:
         Start=int(UCSClist[3])
         End=int(UCSClist[4])
         samfile = pysam.AlignmentFile("chr21.bam", "rb")
-
         reads = []
         count = 0
         for pileupcolumn in samfile.pileup("chr21", Start, End):
@@ -102,7 +101,7 @@ class Assignment1:
                 if pileupread.indel != 0 and pileupread not in reads:
                     reads.append(pileupread)
                     count=count+1
-        print('Reads with indels: ', count)
+        print('Number of Reads with indels: ', count)
         samfile.close()
 
     def calculate_total_average_coverage(self):
@@ -113,7 +112,7 @@ class Assignment1:
             coveragesum=coveragesum+pileupcolumn.n
             length=length+1
         average = coveragesum / length
-        print('Total Coverage: ', average)
+        print('Total Coverage: ', "{:.2f}".format(average))
         samfile.close()
 
     def calculate_gene_average_coverage(self):
@@ -126,7 +125,7 @@ class Assignment1:
         for pileupcolumn in samfile.pileup("chr21", Start, End):
             sum=sum+pileupcolumn.n
         average = sum / length
-        print('Gene Coverage: ', average)
+        print('Gene Coverage: ', "{:.2f}".format(average))
         samfile.close()
 
     def get_number_mapped_reads(self):
@@ -140,9 +139,12 @@ class Assignment1:
 
     def get_region_of_gene(self):
         global UCSClist
-        print("Exonstarts: ", UCSClist[7])
-        print("Exonends: ", UCSClist[8])
-
+        Exon = 1
+        Exonstarts=UCSClist[7].split(',')
+        Exonends=UCSClist[8].split(',')
+        for i in range(0, int(UCSClist[6])):
+            print('Exon', Exon, ': ', Exonstarts[Exon-1], '-', Exonends[Exon-1])
+            Exon +=1
 
     def get_number_of_exons(self):
         global UCSClist
@@ -151,19 +153,19 @@ class Assignment1:
 
     def print_summary(self):
         print("Print all results here")
-        assignment1 = Assignment1()
         UCSClist = []
-        assignment1.download_gene_coordinates('hg38', 'Infofile')
-        assignment1.get_coordinates_of_gene()
-        assignment1.get_gene_symbol()
-        assignment1.get_number_of_exons()
-        assignment1.get_region_of_gene()
-        assignment1.get_sam_header()
-        assignment1.get_number_mapped_reads()
-        assignment1.calculate_gene_average_coverage()
-        assignment1.get_properly_paired_reads_of_gene()
-        assignment1.get_gene_reads_with_indels()
-        assignment1.calculate_total_average_coverage()
+        self.download_gene_coordinates('hg38', 'Infofile')
+        self.get_gene_symbol()
+        self.get_coordinates_of_gene()
+        self.get_number_of_exons()
+        self.get_region_of_gene()
+        self.get_sam_header()
+        self.get_number_mapped_reads()
+        self.calculate_gene_average_coverage()
+        self.calculate_total_average_coverage()
+        self.get_properly_paired_reads_of_gene()
+        self.get_gene_reads_with_indels()
+
 
 def main():
     print("Assignment 1")
